f58c8994e4531efb2cc22a5aacf2a3cb70f5925b
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2023-03-05 21:39:42 +0200

    bank_app

--- lib/peresntation/components/app_bar/appbar_component.dart
+++ lib/peresntation/components/app_bar/appbar_component.dart
@@ -2,8 +2,6 @@ import 'package:bank_app/peresntation/components/Directionality_component.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
-
 class NaturalAppBar extends StatefulWidget implements PreferredSizeWidget {
   dynamic title;
   dynamic color;
@@ -14,25 +12,25 @@ class NaturalAppBar extends StatefulWidget implements PreferredSizeWidget {
   dynamic toolbarHeight;
   dynamic heightFlexibleSpace;
   Widget? leading;
-  dynamic centerTitle ;
-  List<Widget>? widgets ;
+  dynamic centerTitle;
+  dynamic colorIconDrawer = true;
 
+  List<Widget>? widgets;
 
   NaturalAppBar(
       {Key? key,
-        this.title,
-        this.elevation,
-        this.leading,
-        this.child,
-        this.color,
-        this.customSize,
-        this.toolbarHeight,
-        this.centerTitle,
-        this.heightFlexibleSpace,
-        this.widgets,
-        this.colorText})
+      this.title,
+      this.elevation,
+      this.leading,
+      this.child,
+      this.color,
+      this.customSize,
+      this.toolbarHeight,
+      this.centerTitle,
+      this.heightFlexibleSpace,
+      this.widgets,
+      this.colorText})
       : super(key: key);
-
 
   @override
   State<NaturalAppBar> createState() => _NaturalAppBarState();
@@ -42,33 +40,50 @@ class NaturalAppBar extends StatefulWidget implements PreferredSizeWidget {
 }
 
 class _NaturalAppBarState extends State<NaturalAppBar> {
-
-
-
   @override
   Widget build(BuildContext context) {
-       final media = MediaQuery.of(context).size;
+    final media = MediaQuery.of(context).size;
     return DirectionalityComponent(
       child: AppBar(
-        automaticallyImplyLeading: false ,
-        centerTitle: widget.centerTitle == null ? true : false  ,
-        toolbarHeight: widget.toolbarHeight ?? media.height*.1,
+        automaticallyImplyLeading: false,
+        centerTitle: widget.centerTitle == null ? true : false,
+        toolbarHeight: widget.toolbarHeight ?? media.height * .1,
         backgroundColor: Colors.indigo,
         elevation: widget.elevation ?? 15.0,
-        leading: Builder(builder: (context) {
-          return IconButton(
-              onPressed: (){
-                print("hi") ;
-                Scaffold.of(context).openEndDrawer() ;
-              } ,
-              icon: Image.asset(
-                "assets/icons/bell.png",
-                width: media.height*.03,
-                color: Colors.white,
-              )) ;
-        },)  ,
-        actions: widget.widgets ,
-        flexibleSpace: Container(height: widget.heightFlexibleSpace),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+                onPressed: () {
+                  // setState(() {
+                  //   widget.colorIconDrawer = false ;
+                  // });
+                  Scaffold.of(context).openEndDrawer();
+                },
+                icon: Image.asset(
+                  "assets/icons/bell.png",
+                  width: media.height * .03,
+                  color: Colors.white,
+                ));
+          },
+        ),
+        actions: widget.widgets,
+        flexibleSpace: Container(
+          height: widget.heightFlexibleSpace,
+          child: Stack(
+            children: [
+              Positioned(
+                  top: media.height * .12,
+                  right: media.width * .02,
+                  child: Image.asset(
+                    'assets/triangular-arrow.png',
+                    width: media.height * .05,
+                    color: widget.colorIconDrawer == true
+                        ? Colors.indigo
+                        : Colors.indigo.shade900.withOpacity(.9),
+                  ))
+            ],
+          ),
+        ),
         title: Text(
           widget.title,
           style: TextStyle(
--- lib/peresntation/components/app_bar/dad.dart
+++ lib/peresntation/components/app_bar/dad.dart
@@ -1,3 +1,137 @@
+// // import 'dart:ui';
+// //
+// // import 'package:bank_app/peresntation/components/space_component.dart';
+// // import 'package:bank_app/peresntation/components/text_component.dart';
+// // import 'package:flutter/cupertino.dart';
+// // import 'package:flutter/material.dart';
+// // import 'package:flutter_screenutil/flutter_screenutil.dart';
+// //
+// // class DrawerComponent extends StatelessWidget {
+// //   const DrawerComponent({Key? key}) : super(key: key);
+// //
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     final media = MediaQuery.of(context).size;
+// //     return Container(
+// //         width: media.width * .7,
+// //         height: media.height * 2,
+// //         color: Colors.indigo.shade400.withOpacity(.5),
+// //         margin: EdgeInsets.only(top: media.height * .156),
+// //         child: DrawerHeader(
+// //           curve: Curves.bounceInOut,
+// //           padding:EdgeInsets.zero,
+// //           margin: EdgeInsets.zero,
+// //           child: Stack(
+// //             children: [
+// //               Positioned.fill(
+// //                   child: BackdropFilter(
+// //                 filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+// //                 child: const SizedBox(),
+// //               )),
+// //               Positioned(
+// //                 right: 0,
+// //                 child: SizedBox(
+// //                   height: media.height,
+// //                   child: Column(
+// //                     children: [
+// //                       GestureDetector(
+// //                         onTap: () {
+// //                           /// TODO : -----------------
+// //                         },
+// //                         child: Container(
+// //                           width: media.width,
+// //                           height: media.height * .05,
+// //                           margin: EdgeInsets.only(
+// //                               left: media.width * 1,
+// //                               bottom: media.height * .03),
+// //                           decoration: const BoxDecoration(
+// //                               border: Border(
+// //                                   bottom: BorderSide(color: Colors.white))),
+// //                           child: Row(
+// //                             mainAxisAlignment: MainAxisAlignment.end,
+// //                             children: [
+// //                               TextComponent(
+// //                                   text: "حوالة واردة",
+// //                                   fontSize: 18.0,
+// //                                   colorText: Colors.white,
+// //                                   isBold: true),
+// //                               Image.asset(
+// //                                 "assets/black-circle.png",
+// //                                 width: media.width * .2,
+// //                               ),
+// //                             ],
+// //                           ),
+// //                         ),
+// //                       ),
+// //                       GestureDetector(
+// //                         onTap: () {
+// //                           /// TODO : -----------------
+// //                         },
+// //                         child: Container(
+// //                           width: media.width,
+// //                           height: media.height * .05,
+// //                           margin: EdgeInsets.only(
+// //                               left: media.width * 1,
+// //                               bottom: media.height * .03),
+// //                           decoration: const BoxDecoration(
+// //                               border: Border(
+// //                                   bottom: BorderSide(color: Colors.white))),
+// //                           child: Row(
+// //                             mainAxisAlignment: MainAxisAlignment.end,
+// //                             children: [
+// //                               TextComponent(
+// //                                 isBold: true,
+// //                                 text: "تأكيد البريد الالكتروني",
+// //                                 fontSize: 18.0,
+// //                                 colorText: Colors.white,
+// //                               ),
+// //                               Image.asset(
+// //                                 "assets/black-circle.png",
+// //                                 width: media.width * .2,
+// //                               ),
+// //                             ],
+// //                           ),
+// //                         ),
+// //                       ),
+// //                       GestureDetector(
+// //                         onTap: () {
+// //                           /// TODO : -----------------
+// //                         },
+// //                         child: Container(
+// //                           width: media.width,
+// //                           height: media.height * .05,
+// //                           margin: EdgeInsets.only(
+// //                               left: media.width * 1,
+// //                               bottom: media.height * .03),
+// //                           decoration: const BoxDecoration(
+// //                               border: Border(
+// //                                   bottom: BorderSide(color: Colors.white))),
+// //                           child: Row(
+// //                             mainAxisAlignment: MainAxisAlignment.end,
+// //                             children: [
+// //                               TextComponent(
+// //                                 isBold: true,
+// //                                 text: "تم اضافة عملة الجنيه المصري",
+// //                                 fontSize: 18.0,
+// //                                 colorText: Colors.white,
+// //                               ),
+// //                               Image.asset(
+// //                                 "assets/black-circle.png",
+// //                                 width: media.width * .2,
+// //                               ),
+// //                             ],
+// //                           ),
+// //                         ),
+// //                       ),
+// //                     ],
+// //                   ),
+// //                 ),
+// //               ),
+// //             ],
+// //           ),
+// //         ));
+// //   }
+// // }
 // import 'dart:ui';
 //
 // import 'package:bank_app/peresntation/components/space_component.dart';
@@ -6,18 +140,38 @@
 // import 'package:flutter/material.dart';
 // import 'package:flutter_screenutil/flutter_screenutil.dart';
 //
-// class  extends StatelessWidget {
-//   const DrawerComponent({Key? key}) : super(key: key);
+// class DrawerComponent extends StatelessWidget {
+//   dynamic sizeSpace;
+//
+//   DrawerComponent({this.sizeSpace, Key? key}) : super(key: key);
 //
 //   @override
 //   Widget build(BuildContext context) {
 //     final media = MediaQuery.of(context).size;
-//     return Drawer(
-//       backgroundColor: Colors.indigo.shade400.withOpacity(.5),
-//       elevation: 0.0,
-//       // shape: RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.horizontal(end: )),
-//       child:
-//           Stack(
+//     return ListView(
+//       children: [
+//         SpaceComponent(height: media.height*.090,) ,
+//         Container(
+//           alignment: Alignment.bottomRight,
+//           width: media.width * .05,
+//           height: media.height*.03 ,
+//           decoration: const BoxDecoration(
+//             color: Colors.transparent,
+//           ),
+//           child: Row(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               Image.asset('assets/triangular-arrow.png', color: Colors.indigo) ,
+//               SpaceComponent(width: media.width*.04,),
+//             ],
+//           ),
+//         ),
+//         Container(
+//           width: media.width * .8,
+//           height: media.height,
+//           margin: EdgeInsets.only(left: media.width*.3),
+//           color: Colors.indigo.shade300.withOpacity(.4),
+//           child: Stack(
 //             children: [
 //               Positioned.fill(
 //                   child: BackdropFilter(
@@ -25,7 +179,7 @@
 //                     child: const SizedBox(),
 //                   )),
 //               Positioned(
-//                 top: media.height * 0.3,
+//                 top: media.height * .06,
 //                 right: 0,
 //                 child: Column(
 //                   children: [
@@ -33,15 +187,15 @@
 //                       onTap: () {
 //                         /// TODO : -----------------
 //                       },
-//                       child:
-//                       Container(
+//                       child: Container(
 //                         width: media.width,
 //                         height: media.height * .05,
 //                         margin: EdgeInsets.only(
-//                             left: media.width * 1, bottom: media.height * .03),
+//                             left: media.width * 1,
+//                             bottom: media.height * .03),
 //                         decoration: const BoxDecoration(
-//                             border:
-//                             Border(bottom: BorderSide(color: Colors.white))),
+//                             border: Border(
+//                                 bottom: BorderSide(color: Colors.white))),
 //                         child: Row(
 //                           mainAxisAlignment: MainAxisAlignment.end,
 //                           children: [
@@ -49,7 +203,6 @@
 //                                 text: "حوالة واردة",
 //                                 fontSize: 18.0,
 //                                 colorText: Colors.white,
-//
 //                                 isBold: true),
 //                             Image.asset(
 //                               "assets/black-circle.png",
@@ -67,15 +220,16 @@
 //                         width: media.width,
 //                         height: media.height * .05,
 //                         margin: EdgeInsets.only(
-//                             left: media.width * 1, bottom: media.height * .03),
+//                             left: media.width * 1,
+//                             bottom: media.height * .03),
 //                         decoration: const BoxDecoration(
-//                             border:
-//                             Border(bottom: BorderSide(color: Colors.white))),
+//                             border: Border(
+//                                 bottom: BorderSide(color: Colors.white))),
 //                         child: Row(
 //                           mainAxisAlignment: MainAxisAlignment.end,
 //                           children: [
 //                             TextComponent(
-//                               isBold: true ,
+//                               isBold: true,
 //                               text: "تأكيد البريد الالكتروني",
 //                               fontSize: 18.0,
 //                               colorText: Colors.white,
@@ -96,15 +250,16 @@
 //                         width: media.width,
 //                         height: media.height * .05,
 //                         margin: EdgeInsets.only(
-//                             left: media.width * 1, bottom: media.height * .03),
+//                             left: media.width * 1,
+//                             bottom: media.height * .03),
 //                         decoration: const BoxDecoration(
-//                             border:
-//                             Border(bottom: BorderSide(color: Colors.white))),
+//                             border: Border(
+//                                 bottom: BorderSide(color: Colors.white))),
 //                         child: Row(
 //                           mainAxisAlignment: MainAxisAlignment.end,
 //                           children: [
 //                             TextComponent(
-//                               isBold: true ,
+//                               isBold: true,
 //                               text: "تم اضافة عملة الجنيه المصري",
 //                               fontSize: 18.0,
 //                               colorText: Colors.white,
@@ -117,13 +272,14 @@
 //                         ),
 //                       ),
 //                     ),
-//
 //                   ],
 //                 ),
+//
 //               ),
 //             ],
 //           ),
-//
+//         ),
+//       ],
 //     );
 //   }
 // }
